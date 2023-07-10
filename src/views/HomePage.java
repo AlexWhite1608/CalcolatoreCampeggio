bd3e984a32a61739abc5446e369c1fe7753404fe
Alessandro Bianco <alessandro.bianco1@stud.unifi.it>
2023-07-10 15:18:53 +0200

    inseriti file dei menu

--- src/views/HomePage.java
+++ src/views/HomePage.java
@@ -1,4 +1,11 @@
 package views;
 
+import java.awt.*;
+import javax.swing.*;
+
 public class HomePage {
+
+    final static String MENU_CAMPEGGIO = "Campeggio";
+    final static String MENU_MARGHERITA = "Margherita";
+    final static String MENU_PREZZARIO = "Prezzario";
 }
