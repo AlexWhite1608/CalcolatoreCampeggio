ac55bc6ebe30bb09b29eca743f424366ca036de4
Alessandro Bianco <alessandro.bianco1@stud.unifi.it>
2023-07-12 22:19:44 +0200

    scritti correttamente layout datepickers e num notti in menu campeggio (codice)

--- src/main/java/views/MenuCampeggio.java
+++ src/main/java/views/MenuCampeggio.java
@@ -1,6 +1,5 @@
 package views;
 
-import org.jdatepicker.JDatePicker;
 import org.jdatepicker.impl.JDatePanelImpl;
 import org.jdatepicker.impl.JDatePickerImpl;
 import org.jdatepicker.impl.UtilDateModel;
@@ -14,6 +13,13 @@ import java.util.Properties;
 
 public class MenuCampeggio extends JPanel {
     private JPanel mainPanel;
+    private JPanel pnDatePickers;
+    private JPanel pnDatePickerArrivo;
+    private JPanel pnDatePickerPartenza;
+    private JPanel pnNotti;
+
+    private GridLayout mainGridLayout;
+
     private JTextField tfNumNotti;
     private JTextField tfNumAdulti;
     private JTextField tfNoTax;
@@ -21,7 +27,6 @@ public class MenuCampeggio extends JPanel {
     private JTextField tfNumAnimali;
     private JRadioButton rbCamper;
     private JRadioButton rbTenda;
-    private JLabel numNottiLabel;
     private JLabel numAdultiLabel;
     private JLabel numBambiniLabel;
     private JLabel numAnimaliLabel;
@@ -37,24 +42,86 @@ public class MenuCampeggio extends JPanel {
     private JButton btnCancella;
     private JPanel pnlButtons;
     private JTextField tfExtra;
-    private JPanel pnDatePickerArrivo;
-    private JPanel pnDatePickerPartenza;
-    private JDatePicker dpArrivo;
-    private JDatePicker dpPartenza;
+
 
     public MenuCampeggio() {
+        // Layout principale
+        mainPanel = new JPanel();
+        mainPanel.setLayout(new GridBagLayout());
 
-        //Inizializzazione
+        // Inizializzazione pannelli
         datePickersInitializer();
-        clearFormInitializer();
+        numNottiInitializer();
+        //clearFormInitializer();
 
         add(mainPanel);
-
         setVisible(true);
-
     }
 
-    //Codice per cancellare tutto il form
+    // Inizializzazione date pickers
+    private void datePickersInitializer() {
+        Properties p = new Properties();
+        p.put("text.today", "Today");
+        p.put("text.month", "Month");
+        p.put("text.year", "Year");
+        UtilDateModel model = new UtilDateModel();
+
+        // Date picker arrivo
+        pnDatePickerArrivo = new JPanel();
+        pnDatePickerArrivo.setLayout(new FlowLayout());
+        JDatePanelImpl datePanelArrivo = new JDatePanelImpl(model, p);
+        JDatePickerImpl datePickerArrivo = new JDatePickerImpl(datePanelArrivo, new CustomDateLabelFormatter());
+        pnDatePickerArrivo.add(new JLabel("Arrivo:"));
+        pnDatePickerArrivo.add(datePickerArrivo);
+
+        // Date picker partenza
+        pnDatePickerPartenza = new JPanel();
+        pnDatePickerPartenza.setLayout(new FlowLayout());
+        JDatePanelImpl datePanelPartenza = new JDatePanelImpl(model, p);
+        JDatePickerImpl datePickerPartenza = new JDatePickerImpl(datePanelPartenza, new CustomDateLabelFormatter());
+        pnDatePickerPartenza.add(new JLabel("Partenza:"));
+        pnDatePickerPartenza.add(datePickerPartenza);
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        mainPanel.add(pnDatePickerArrivo, gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        mainPanel.add(pnDatePickerPartenza, gbc);
+    }
+
+    // Inizializza panel numero notti
+    private void numNottiInitializer() {
+        pnNotti = new JPanel();
+        pnNotti.setLayout(new FlowLayout());
+
+        JPanel pnNumNotti = new JPanel();
+        pnNumNotti.setLayout(new FlowLayout());
+        pnNumNotti.add(new JLabel("N° Notti"));
+        tfNumNotti = new JTextField(10);
+        tfNumNotti.setEditable(false);
+        pnNumNotti.add(tfNumNotti);
+
+        JPanel pnAsBs = new JPanel();
+        pnAsBs.setLayout(new FlowLayout(FlowLayout.LEFT, 50, 0));
+        altaStagioneLabel = new JLabel("AS: ");
+        bassaStagioneLabel = new JLabel("BS: ");
+        pnAsBs.add(altaStagioneLabel);
+        pnAsBs.add(bassaStagioneLabel);
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        mainPanel.add(pnNumNotti, gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        mainPanel.add(pnAsBs, gbc);
+    }
+
+    // Codice per cancellare tutto il form
     private void clearFormInitializer(){
         btnCancella.addActionListener(new ActionListener() {
             @Override
@@ -66,32 +133,6 @@ public class MenuCampeggio extends JPanel {
                 tfNoTax.setText("");
             }
         });
-    }
-
-    //Inizializzazione date pickers
-    private void datePickersInitializer(){
-        pnDatePickerArrivo.setLayout(new FlowLayout());
-        pnDatePickerPartenza.setLayout(new FlowLayout());
-        Properties p = new Properties();
-        p.put("text.today", "Today");
-        p.put("text.month", "Month");
-        p.put("text.year", "Year");
-        UtilDateModel model = new UtilDateModel();
-
-        JDatePanelImpl datePanelArrivo = new JDatePanelImpl(model, p);
-        JDatePickerImpl datePickerArrivo = new JDatePickerImpl(datePanelArrivo, new CustomDateLabelFormatter());
-
-        JDatePanelImpl datePanelPartenza = new JDatePanelImpl(model, p);
-        JDatePickerImpl datePickerPartenza = new JDatePickerImpl(datePanelPartenza, new CustomDateLabelFormatter());
-
-        pnDatePickerArrivo.add(new JLabel("DIOCANE"));
-        pnDatePickerArrivo.add(datePickerArrivo);
-
-        pnDatePickerPartenza.add(new JLabel("DIOMERDA"));
-        pnDatePickerPartenza.add(datePickerPartenza);
-
-        add(pnDatePickerArrivo);
-        add(pnDatePickerPartenza);
     }
 
 }
