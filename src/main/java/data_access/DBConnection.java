f60980df3c2faf4129458d1288f0b99242770b92
Alessandro Bianco <alessandro.bianco1@stud.unifi.it>
2023-07-13 10:07:08 +0200

    inizializzate classi gestione database prezzario

---
+++ src/main/java/data_access/DBConnection.java
@@ -0,0 +1,14 @@
+package data_access;
+
+public class DBConnection {
+
+    // Connessione database
+    public static void connect(){
+
+    }
+
+    // Disconnessione database
+    public static void disconnect(){
+
+    }
+}
