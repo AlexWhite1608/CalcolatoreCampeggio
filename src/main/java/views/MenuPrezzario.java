d973bd6596ce49c5943aa200dfcd9c644e3c5302
Alessandro Bianco <alessandro.bianco1@stud.unifi.it>
2023-07-14 11:30:11 +0200

    impostata data tabella

--- src/main/java/views/MenuPrezzario.java
+++ src/main/java/views/MenuPrezzario.java
@@ -4,6 +4,7 @@ import data_access.PrezzarioGateway;
 import data_access.PrezzarioTableModel;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import javax.swing.table.JTableHeader;
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -27,16 +28,33 @@ public class MenuPrezzario extends JPanel {
 
     // Inizializza la tabella
     private void initTable() throws URISyntaxException, IOException {
-        PrezzarioGateway prezzarioGateway = new PrezzarioGateway();
         pnTable = new JPanel();
         tblPrezzi = new JTable();
 
-        pnTable.add(new JScrollPane(tblPrezzi));
+        // Crea il modello della tabella con i dati e i nomi delle colonne
+        Object[][] data = {
+                {"Camper", 13.0, 16.0},
+                {"Tenda", 9.0, 12.0},
+                {"Adulti", 8.0, 10.0},
+                {"Bambini (3-10)", 4.5, 6.0},
+                {"Animali", 3.0, 3.0},
+                {"Tassa di soggiorno (>12)", 1.5, 1.5}
+        };
+        String[] columnNames = {"Tipologia", "Bassa Stagione", "Alta Stagione"};
+        DefaultTableModel tableModel = new DefaultTableModel(data, columnNames);
+        tblPrezzi.setModel(tableModel);
 
-        tblPrezzi.setModel(new PrezzarioTableModel(prezzarioGateway.getValues()));
-        tblPrezzi.setTableHeader(new JTableHeader());
+        // Crea l'header della tabella
+        JTableHeader tableHeader = tblPrezzi.getTableHeader();
+
+        // Aggiungi la tabella e l'header al pannello
+        pnTable.add(new JScrollPane(tblPrezzi));
+        pnTable.add(tableHeader);
+
         tblPrezzi.setRowHeight(50);
+        add(pnTable);
     }
+
 
 
     private void createUIComponents() throws URISyntaxException, IOException {
