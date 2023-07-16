3f402284cac2dc7b64b1a4257b5d2a91cd4e9fa9
Alessandro Bianco <alessandro.bianco1@stud.unifi.it>
2023-07-16 10:44:00 +0200

    adesso l'applicazione viene lanciata correttamente nel main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,19 @@ import views.HomePage;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import javax.swing.SwingUtilities;
 
 public class Main {
     public static void main(String[] args) throws URISyntaxException, IOException {
-        HomePage homePage = new HomePage();
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    new HomePage();
+                } catch (URISyntaxException | IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
     }
 }
